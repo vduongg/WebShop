5ed9253536b6b88c0cb273488652c302064773fc
vduongg <137416423+vduongg@users.noreply.github.com>
2023-08-24 20:08:34 +0700

    required

---
+++ KaiserStore/KaiserStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KaiserStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
